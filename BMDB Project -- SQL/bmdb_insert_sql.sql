d78dd51ba64d54b22dbba59834b4c4d596232ea0
Brandon Moore <preacherman65@gmail.com>
2024-08-26 11:03:34 -0400

    changed some data

--- BMDB Project -- SQL/bmdb_insert_sql.sql
+++ BMDB Project -- SQL/bmdb_insert_sql.sql
@@ -1,5 +1,8 @@
 --bmbd CRUD stuff
 -- Insert a Movie
+USE bmdb;
+GO
+
 INSERT INTO Movie (Title, Year, Rating, Director)
 VALUES
 	('The Princess Bride',	1987,	'PG',		'Rob Reiner'),
@@ -11,23 +14,35 @@ VALUES
 -- insert Actor into Actor table
 INSERT INTO Actor (FirstName, LastName, Gender, Birthdate)
 VALUES
-	--('Cary',		'Elwes',		'M', '1962-10-26'),
+	('Cary',		'Elwes',		'M', '1962-10-26'),
 	('Brendan',		'Fraser',		'M', '1968-12-03'),
 	('John',		'Candy',		'M', '1950-10-31'),
 	('Viggo',		'Mortensen',	'M', '1958-10-20'),
-	('Chevy',		'Chase',		'M', '1943-10-8');
+	('Chevy',		'Chase',		'M', '1943-10-8'),
+	('Mandy',		'Patinkin',		'M', '1952-11-30'),
+	('Rachel',		'Weisz',		'F', '1970-03-07'),
+	('Dan',			'Aykroyd',		'M', '1952-07-01'),
+	('Ian',			'McKellan',		'M', '1939-05-25'),
+	('Randy',		'Quaid',		'M', '1950-10-01');
+
 
 
 INSERT INTO Credit (MovieId, ActorId, Role)
 VALUES
-	--(1, 1, 'Westley')
+	(1, 1, 'Westley'),
 	(3, 3, 'Chet Ripley'),
 	(2, 2, 'Rick O''Connell'),
 	(4, 4, 'Aragorn'),
-	(5, 5, 'Clark Griswold');
+	(5, 5, 'Clark Griswold'),
+	(1, 6, 'Inigo Montoya'),
+	(2, 7, 'Evelyn O''Connell'),
+	(3, 8, 'Roman Craig'),
+	(4, 9, 'Gandalf'),
+	(5, 10, 'Cousin Eddie');
 
 
-SELECT * 
+
+/*SELECT * 
 FROM Movie;
 
 --update statement
@@ -45,4 +60,4 @@ DELETE
 SELECT Title, Year, FirstName, LastName, Role
   FROM Credit C
   JOIN Movie M ON C.MovieId = M.Id
-  JOIN Actor A ON C.ActorId = A.ID;
+  JOIN Actor A ON C.ActorId = A.ID;*/
